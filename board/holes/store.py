844bb77fc123e353bbe5f0eb2ea3a7b354d7af22
Dan Katzuv <dan.dan.kafe@gmail.com>
2019-03-23 23:26:12 +0200

    Capitalize side of store

--- board/holes/store.py
+++ board/holes/store.py
@@ -4,7 +4,7 @@ from board.holes.hole import Hole
 class Store(Hole):
     def __init__(self, side: str):
         super().__init__(side)
-        self.text = f'{self.side} store\n0'
+        self.text = f'{self.side.capitalize()} store: 0'
 
     def update(self, updated_amount: int):
-        self.text = f'{self.side} store: {updated_amount}'
+        self.text = f'{self.side.capitalize()} store: {updated_amount}'
